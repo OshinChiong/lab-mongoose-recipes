bab3394e741539892b2c3abdf9aa2c37c91ec896
Oshin Chiong <oshinchiong@Oshins-MacBook-Air.local>
2022-04-13 17:51:18 -0400

    half

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+let newRecipe = { 
+   title: "Pomodoro",
+  level:  "Amateur Chef",
+  ingredients: ["tomato" , "pasta"], 
+  cuisine: "Italian",
+  dishType:  "main_course",
+  image: "https://thestayathomechef.com/wp-content/uploads/2020/04/Pasta-Pomodoro-2-scaled.jpg",
+  duration: 20,
+  creator: "Me" 
+};
+
+Recipe
+  .create(newRecipe)
+  .then(function (results) {
+    console.log("Success!", results);
+  })
+  .catch(function (err) {
+    console.log("Something went wrong", err.message);
+  });
+  
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
